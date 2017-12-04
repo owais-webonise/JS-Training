7554f953492836ec9bdcbf2166697371670333b5
owais-webonise <owais.kazi@weboniselab.com>
2017-12-04 16:33:04 +0530

    Add ToDo assignment with React JS - Main Component - Parent Component - Add(Child) Component - Search(Child) Component

---
+++ my-app/src/searchBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+     super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        {this.props.name}
+      </div>
+    );
+  }
+}
+
+export default SearchBar;

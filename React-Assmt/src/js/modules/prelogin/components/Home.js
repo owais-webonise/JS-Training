46dac86630d8214f1924824fd40581f939c28b8c
owais-webonise <owais.kazi@weboniselab.com>
2017-12-18 19:42:16 +0530

    Update Student React App - Add multicheck - Add enum for checkbox

--- React-Assmt/src/js/modules/prelogin/components/Home.js
+++ React-Assmt/src/js/modules/prelogin/components/Home.js
@@ -2,22 +2,25 @@ class Home extends React.Component {
     render() {
       console.log(this.props.location.query.param1)
         return (
-          <div> Name:
-          {this.props.location.query.param1}
-          {this.props.location.query.param2}
-            <th> English </th>
-            <th> Hindi </th>
-            <th> Mathematics </th>
-            <th> Total</th>
-            <th> Percentage </th>
-            <div>
-              <td> {this.props.location.query.param3} </td>
-              <td> {this.props.location.query.param4} </td>
-              <td> {this.props.location.query.param5} </td>
-              <td> {this.props.location.query.param6} </td>
-              <td> {this.props.location.query.param7} </td>
-            </div>
-          </div>
+          <div>
+            <span className="studentName">Name:{this.props.location.query.param1}  {this.props.location.query.param2}</span>
+            <table className="studentDetails table">
+            <tr>
+              <th> English </th>
+              <th> Hindi </th>
+              <th> Mathematics </th>
+              <th> Total</th>
+              <th> Percentage </th>
+             </tr>
+              <tr>
+                <td> {this.props.location.query.param3} </td>
+                <td> {this.props.location.query.param4} </td>
+                <td> {this.props.location.query.param5} </td>
+                <td> {this.props.location.query.param6} </td>
+                <td> {this.props.location.query.param7} </td>
+              </tr>
+            </table>
+         </div>
         );
     }
 }
--- React-Assmt/src/js/modules/prelogin/components/checkbox.js
+++ React-Assmt/src/js/modules/prelogin/components/checkbox.js
@@ -1,64 +1,53 @@
 import React, {Component} from 'react';
+import {checkboxEnum, ResultType} from './constants.js';
 
+var checkValue = [];
+var uncheckValue = [];
 class Checkbox extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       newResults: this.props.allResults
     }
-    this._changecheck = this._changecheck.bind(this);
   }
 
-_changecheck(event) {
-  if(event.target.id === "distinction") {
+  _changecheck(marks) {
     var filteredData = Object.assign([],this.state.newResults);
+    var min = checkboxEnum[marks].min;
+    var max = checkboxEnum[marks].max;
     filteredData = filteredData.filter(function (index) {
-      if(index.percentage >= 60) {
+      if(index.percentage >= min && index.percentage < max) {
         return index.firstName;
       }
     })
+    if(event.target.id === marks && event.target.checked) {
+      checkValue = _.uniq(filteredData.concat(checkValue));
+      this.props.handleCheck(checkValue);
+    }
+    if(event.target.id === marks && event.target.checked == false) {
+      checkValue = _.difference(checkValue,filteredData);
+      if(checkValue.length) {
+      this.props.handleCheck(checkValue);
+     } else {
+      this.props.handleCheck(this.state.newResults);
+     }
+   }
   }
-   if(event.target.id === "first" ) {
-    var filteredData = Object.assign([],this.state.newResults);
-    filteredData = filteredData.filter(function (index) {
-      if(index.percentage >= 50 && index.percentage < 60) {
-        return index.firstName;
-      }
-    })
-   }
-   if(event.target.id === "second") {
-    var filteredData = Object.assign([],this.state.newResults);
-    filteredData = filteredData.filter(function (index) {
-      if(index.percentage >= 40 && index.percentage < 50) {
-        return index.firstName;
-      }
-    })
-   }
-   if(event.target.id === "fail") {
-    var filteredData = Object.assign([],this.state.newResults);
-    filteredData = filteredData.filter(function (index) {
-      if(index.percentage < 40) {
-        return index.firstName;
-      }
-    })
-   }
-   if(event.target.checked == false) {
-    var filteredData = Object.assign([],this.state.newResults);
-   }
-  this.props.handleCheck(filteredData);
-}
 
   render() {
     return (
-      <div>
-        <input type = "checkbox" onChange = {this._changecheck} id="distinction"/>
-        <label for="distinction">Distinction</label>
-        <input type = "checkbox" onChange = {this._changecheck} id="first"/>
-        <label for="first">First Class</label>
-        <input type = "checkbox" onChange = {this._changecheck} id="second"/>
-        <label for="second">Second Class</label>
-        <input type = "checkbox" onChange = {this._changecheck} id="fail"/>
-        <label for="fail">Fail</label>
+      <div className="clearfix">
+      {
+        _.map(ResultType, (item, key) => {
+          return (
+            <div key={key} className="pull-left">
+            <input type = "checkbox" onChange = {this._changecheck.bind(this,item)} id={item}/>
+            <label for={item}>{item}</label>
+          </div>
+            )
+        })
+
+      }
       </div>
     );
   }
